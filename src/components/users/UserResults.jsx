606785058a3ddcfe84defc3e11183694d3cb4bd0
Cyrille Bomba <cbngong@gmail.com>
2022-04-13 22:30:43 +0200

    Display users

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
+
+const UserResults = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const response = await fetch(`https://api.github.com/users`);
+    const data = await response.json();
+    setUsers(data);
+    setLoading(false);
+  };
+
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <UserItem user={user} />
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
+};
+
+export default UserResults;
